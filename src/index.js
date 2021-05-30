8d65a6cc3f81c82eb926178d522d65e0b5bd0f56
Christian Lisangola <jochri3@gmail.com>
2021-05-30 22:20:09 +0100

    createStore:add initial state to the reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { createStore } from "redux";
+import { v4 as uuiv4 } from "uuid";
 
-const reducer = () => {
-  return ["a", "b", "c", "ef"];
+const initialState = [
+  { id: uuiv4(), todo: "Manger" },
+  { id: uuiv4(), todo: "Aller à la salle de Gymn" },
+];
+
+const reducer = (state = initialState, action) => {
+  return state;
 };
 
 const store = createStore(reducer);
