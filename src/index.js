9d81aa95d071541144fc0f722700f282e81897a8
Christian Lisangola <jochri3@gmail.com>
2021-05-31 07:26:03 +0100

    createStore:add basic reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import { createStore } from "redux";
 import { v4 as uuiv4 } from "uuid";
 
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "ADD_TODO";
+
+const addTodo = (name) => {
+  return {
+    type: ADD_TODO,
+    payload: { name },
+  };
+};
+const removeTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    payload: { id },
+  };
+};
+
 const initialState = [
   { id: uuiv4(), todo: "Manger" },
   { id: uuiv4(), todo: "Aller à la salle de Gymn" },
 ];
 
 const reducer = (state = initialState, action) => {
+  if (action.type === ADD_TODO) {
+    return [...state, { id: uuiv4(), name: action.payload.name }];
+  }
+  if (action.type === DELETE_TODO) {
+    return state.filter((todo) => todo.id !== action.payload.id);
+  }
   return state;
 };
 
