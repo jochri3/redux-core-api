d9ff9e49c18852b72b3f9660fdf552b10198de49
Christian Lisangola <jochri3@gmail.com>
2021-06-02 07:30:18 +0100

    refactor:change state todos array to object with attributes

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -1,20 +1,31 @@
 import { v4 as uuiv4 } from "uuid";
 import { ADD_TODO, DELETE_TODO } from "../action.types";
 
-const initialState = [
-  { id: uuiv4(), todo: "Manger", completed: false },
-  { id: uuiv4(), todo: "Aller à la salle de Gymn", completed: false },
-];
+const initialState = {
+  todos: [
+    { id: uuiv4(), todo: "Manger", completed: false },
+    { id: uuiv4(), todo: "Aller à la salle de Gymn", completed: false },
+  ],
+  todoForm: { todoName: "" },
+};
 
 export const reducer = (state = initialState, action) => {
   if (action.type === ADD_TODO) {
-    return [
+    return {
       ...state,
-      { id: uuiv4(), todo: action.payload.todo, completed: false },
-    ];
+      todos: {
+        ...state.todos,
+        id: uuiv4(),
+        todo: action.payload.todo,
+        completed: false,
+      },
+    };
   }
   if (action.type === DELETE_TODO) {
-    return state.filter((todo) => todo.id !== action.payload.id);
+    return {
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== action.payload.id),
+    };
   }
   return state;
 };
