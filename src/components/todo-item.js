039a764ec42e95a4da839f9567fcb6b93dfe03e9
Christian Lisangola <jochri3@gmail.com>
2021-06-03 16:54:36 +0100

    feact:attach view event listener to view button

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -1,4 +1,5 @@
 import { deleteTodo, toggleTodo } from "../store/todos/actions";
+import { selectTodo } from "../store/todos/actions/todo.action";
 
 export const TodoItem = ({ id, todo, completed }) => {
   const li = document.createElement("li");
@@ -10,14 +11,21 @@ export const TodoItem = ({ id, todo, completed }) => {
 
   li.appendChild(todoName);
 
+  //view button
+  const viewButton = document.createElement("button");
+  const viewButtonName = document.createTextNode("Voir");
+  viewButton.appendChild(viewButtonName);
+  li.appendChild(viewButton);
+  viewButton.addEventListener("click", store.dispatch(selectTodo(id)));
+
   //button
   const deleteButton = document.createElement("button");
   const deleteButtonName = document.createTextNode("X");
+  deleteButton.appendChild(deleteButtonName);
   li.appendChild(deleteButton);
   li.addEventListener("click", () => store.dispatch(toggleTodo(id)));
 
   //add delete methode
-  deleteButton.appendChild(deleteButtonName);
   deleteButton.addEventListener("click", () => store.dispatch(deleteTodo(id)));
   return li;
 };
