cab0c79f8a0d60e19d3597d1d0c5bf303b37d7fd
Christian Lisangola <jochri3@gmail.com>
2021-06-03 09:27:52 +0100

    refactor:create separate form-reducer

---
+++ src/store/reducers/form-reducer.js
@@ -0,0 +1,17 @@
+import { FORM_RESET, FORM_UPDATE_INPUT_FIELD } from "../action.types";
+
+const initialState = {
+  todoForm: { todoName: "" },
+};
+
+export const reducer = (state = initialState, action) => {
+  if (action.type === FORM_UPDATE_INPUT_FIELD) {
+    return { ...state, todoForm: { todoName: action.payload.value } };
+  }
+
+  if (action.type === FORM_RESET) {
+    return { ...state, todoForm: { todoName: "" } };
+  }
+
+  return state;
+};
