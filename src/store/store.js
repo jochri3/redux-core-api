6b42d3ce70e2e26d0f0f00da041b8fd874904053
Christian Lisangola <jochri3@gmail.com>
2021-06-04 01:48:05 +0100

    feat:add simple logger enhancer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,22 @@
-import { createStore } from "redux";
+import { compose, createStore } from "redux";
 import { rootReducer } from "./root-reducer";
+
+//initialState,enhances
+const logger = (createStore) => (reducer) => {
+  const loggerReducer = (state, action) => {
+    console.log("prevstate : ", state);
+    const newState = reducer(state, action);
+    console.log("nextstate : ", newState);
+    return newState;
+  };
+  return createStore(loggerReducer);
+};
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  logger
+
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 window.store = store;
