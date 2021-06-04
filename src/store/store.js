01f792adaf48b3c0adceff6e0be32d359ac0da7d
Christian Lisangola <jochri3@gmail.com>
2021-06-04 04:24:15 +0100

    feat:configure enhancer according to environment NODE_ENV

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,21 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { asyncMiddleware } from "./middlewares/async";
 import { rootReducer } from "./root-reducer";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
-
-
+const middlewares = [asyncMiddleware];
+if (process.env.NODE_ENV !== "production") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(asyncMiddleware, logger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middlewares),
+    process.env.NODE_ENV !== "production" &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
